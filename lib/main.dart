f7d6ba1b53c689ae8303c6788d6b749608058b69
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2022-05-21 22:20:25 +0530

    Icon_change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
